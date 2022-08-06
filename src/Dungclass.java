8b65cc3d1ab24d489f4044fc89bcdb39c777506c
DELL <nguyennavi0611@gmail.com>
2022-08-06 19:40:57 +0700

    git push

--- src/Dungclass.java
+++ src/Dungclass.java
@@ -1,6 +1,10 @@
+package src;
+
 public class Dungclass {
     public static void main (String args) {
         System.out.println("Dung xin chao!!");
+        System.out.println("Bai tap moi");
+
     }
 
 }
